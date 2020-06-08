fc55e529bf28e1f8cc27263a5eff64b7ea71e6ae
Mammoth55 <slamam@mail.ru>
2020-06-09 01:13:49 +0600

    4.13 Домашняя работа 4.2

---
+++ 04_NumbersStringsAndDates/Test/src/main/java/Test.java
@@ -0,0 +1,11 @@
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("Double  " + -Double.MAX_VALUE + "\t" + Double.MAX_VALUE);
+        System.out.println("Float  " + -Float.MAX_VALUE + "\t" + Float.MAX_VALUE);
+        System.out.println("Long  " + Long.MIN_VALUE + "\t" + Long.MAX_VALUE);
+        System.out.println("Int  " + Integer.MIN_VALUE + "\t" + Integer.MAX_VALUE);
+        System.out.println("Short  " + Short.MIN_VALUE + "\t" + Short.MAX_VALUE);
+        System.out.println("Byte  " + Byte.MIN_VALUE + "\t" + Byte.MAX_VALUE);
+    }
+}
