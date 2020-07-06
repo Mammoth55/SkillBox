cf40e770a5d66ee791c486cff85d2f76eeef9f56
Mammoth55 <slamam@mail.ru>
2020-07-06 22:23:46 +0600

    5.11 Домашняя работа 5.4

--- 05_ArraysAndCollections/src/main/java/ArrayListSystem.java
+++ 05_ArraysAndCollections/src/main/java/ArrayListSystem.java
@@ -4,9 +4,9 @@ import java.util.regex.Pattern;
 
 public class ArrayListSystem {
 
-    private static Set<String> eMail = new TreeSet<>();
+    private static Map<String, String> phoneBook = new TreeMap<>();
 
-    public static boolean checkEmail(String str) {
+    private static boolean isEmail(String str) {
         Pattern pattern = Pattern.compile("\\b(.+)@(.+)\\b");
         Matcher matcher = pattern.matcher(str);
         boolean status = false;
@@ -16,36 +16,91 @@ public class ArrayListSystem {
         return status;
     }
 
+    private static String phoneDetect(String str) {
+        String s = str.replaceAll("[^0-9]", "");
+        int num = s.length();
+        if (num < 10 || num > 11 || (num == 11 && s.charAt(0) != '8' && s.charAt(0) != '7')) {
+            s = "";
+        } else {
+            if (num == 11) {
+                s = s.substring(1);
+            }
+            s = "7" + s;
+        }
+        return s;
+    }
+
+    private static String nameDetect(String str) {
+        String s;
+        Pattern pattern = Pattern.compile("\\b[a-zA-Zа-яА-Я]+");
+        Matcher matcher = pattern.matcher(str);
+        s = matcher.find() ? matcher.group() : "";
+        return s;
+    }
+
+    private static void whenNameReceived(String inputName) {
+        boolean isExist = false;
+        for (String num : phoneBook.keySet()) {
+            if (phoneBook.get(num).equals(inputName)) {
+                printCard(num);
+                isExist = true;
+                break;
+            }
+        }
+        if (!isExist) {
+            String input;
+            do {
+                System.out.println("ENTER Phone for new User :");
+                Scanner in = new Scanner(System.in);
+                input = phoneDetect(in.nextLine());
+            } while (input.equals(""));
+            phoneBook.put(input, inputName);
+        }
+    }
+
+    private static void whenPhoneReceived(String num) {
+        if (phoneBook.keySet().contains(num)) {
+            printCard(num);
+        } else {
+            String input;
+            do {
+                System.out.println("ENTER Name for new User :");
+                Scanner in = new Scanner(System.in);
+                input = nameDetect(in.nextLine());
+            } while (input.equals(""));
+            phoneBook.put(num, input);
+        }
+    }
+
+    private static void printCard(String num) {
+        System.out.println("Phone : " + num + "\t" + " Name : " + phoneBook.get(num));
+    }
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        boolean dontExit = true;
-        while (dontExit) {
-            System.out.print("Введите команду (LIST/ADD/EXIT) : ");
-            List<String> input = new ArrayList<>();
-            for (String str : in.nextLine().trim().split("\\s+", 2)) {
-                input.add(str);
-            }
-            switch (input.get(0)) {
+        boolean isLoop = true;
+        while (isLoop) {
+            System.out.print("Enter command 'LIST'/'EXIT' or your NAME/NUMBER : ");
+            String input = in.nextLine().trim();
+            switch (input) {
                 case "LIST":
-                    System.out.println("EMAIL LIST :");
-                    int index = 0;
-                    for (String s : eMail) {
-                        System.out.println(index++ + "\t" + s);
-                    }
-                    break;
-                case "ADD":
-                    if (input.size() == 1 || !checkEmail(input.get(1))) {
-                        System.out.println("Wrong ADD format !");
-                    } else {
-                        eMail.add(input.get(1));
-                        System.out.println("Done.");
+                    System.out.println("PHONE LIST :");
+                    for (String num : phoneBook.keySet()) {
+                        String name = phoneBook.get(num);
+                        System.out.println(num + " => " + name);
                     }
                     break;
                 case "EXIT":
-                    dontExit = false;
+                    isLoop = false;
                     break;
                 default:
-                    System.out.println("Неверная команда !");
+                    String number = phoneDetect(input);
+                    if (number.equals("")) {
+                        whenNameReceived(input);
+                    } else {
+                        whenPhoneReceived(number);
+                    }
+                    System.out.println("Done.");
                     break;
             }
         }
