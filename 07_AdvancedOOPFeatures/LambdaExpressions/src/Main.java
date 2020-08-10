9da6f59fbe03032d5cd3d788f3f37eed02349c95
Mammoth55 <slamam@mail.ru>
2020-08-11 00:08:14 +0600

    7.2 Домашняя работа

--- 07_AdvancedOOPFeatures/LambdaExpressions/src/Main.java
+++ 07_AdvancedOOPFeatures/LambdaExpressions/src/Main.java
@@ -5,23 +5,15 @@ import java.text.SimpleDateFormat;
 import java.util.*;
 import java.util.stream.Stream;
 
-public class Main
-{
-    private static String staffFile = "data/staff.txt";
+public class Main {
+    
+    private static String staffFile
+            = "C:\\projects\\SkillBox\\java_basics\\07_AdvancedOOPFeatures\\LambdaExpressions\\data\\staff.txt";
     private static String dateFormat = "dd.MM.yyyy";
 
-    public static void main(String[] args)
-    {
-        ArrayList<Employee> staff = loadStaffFromFile();
-
-
-    }
-
-    private static ArrayList<Employee> loadStaffFromFile()
-    {
+    private static ArrayList<Employee> loadStaffFromFile() {
         ArrayList<Employee> staff = new ArrayList<>();
-        try
-        {
+        try {
             List<String> lines = Files.readAllLines(Paths.get(staffFile));
             for(String line : lines)
             {
@@ -41,5 +33,16 @@ public class Main
             ex.printStackTrace();
         }
         return staff;
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Employee> staff = loadStaffFromFile();
+        Collections.sort(staff, (e1, e2) -> {
+            int rsl = e1.getSalary().compareTo(e2.getSalary());
+            return rsl != 0 ? rsl : e1.getName().compareTo(e2.getName());
+        });
+        for (Employee emp: staff) {
+            System.out.println(emp);
+        }
     }
 }
