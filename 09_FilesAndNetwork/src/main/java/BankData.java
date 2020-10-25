a648df7d05467a64d89a1c33cd3178d87213a574
Mammoth55 <slamam@mail.ru>
2020-10-25 22:44:38 +0600

    9.9 Домашняя работа 9.3 (advanced level)

--- 09_FilesAndNetwork/src/main/java/BankData.java
+++ 09_FilesAndNetwork/src/main/java/BankData.java
@@ -1,61 +1,117 @@
+import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class BankData {
 
-    private static String dataFile = "files/movementList.csv";
-    private static double out = 0, in = 0;
-    private static Map<String, Double> shops = new HashMap<>();
+    private static String dataFile = "files/movementList.org";
+    private static List<Transaction> transactions = new ArrayList<>();
+    private static HashMap<String, Double> shops = new HashMap<>();
 
     public static void main(String[] args) {
         try {
-            List<String> lines = Files.readAllLines(Paths.get(dataFile));
-            lines.remove(0);
-            double currentOut;
-            int len;
-            for (String line : lines) {
-                String[] fragments = line.split(",");
-                len = fragments.length;
-                if (len != 8) {
-                    if (len == 9) {
-                        fragments[7] = fragments[7].substring(1) + "."
-                                + fragments[8].substring(0, fragments[8].length() - 2);
-                    } else {
-                        System.out.println("Wrong line: " + line);
-                        continue;
-                    }
-                }
-                currentOut = Double.parseDouble(fragments[7]);
-                out += currentOut;
-                in += Double.parseDouble(fragments[6]);
-                if (currentOut > 0) {
-                    String[] segments = fragments[5].split("    ");
-                    String[] words = segments[1].split("[\\/\\\\]");
-                    StringBuilder builder = new StringBuilder();
-                    for (int i = 1; i < words.length; i++) {
-                        builder.append(words[i]);
-                        builder.append("\\");
-                    }
-                    String str = builder.toString();
-                    if (shops.keySet().contains(str)) {
-                        shops.put(str, shops.get(str) + currentOut);
-                    } else {
-                        shops.put(str, new Double(currentOut));
-                    }
-                }
+            Path dataPath = Paths.get(dataFile);
+            if (dataFile.endsWith(".csv")) {
+                transactions = parseCSV(dataPath);
+            } else if (dataFile.endsWith(".org")) {
+                transactions = parseORG(dataPath);
+            } else {
+                throw new RuntimeException("Unknown file type : " + dataFile);
             }
+            printSumOfIncome(transactions);
+            printSumOfExpenses(transactions);
+            printSummaryByGroups(transactions);
         } catch (Exception ex) {
             ex.printStackTrace();
         }
-        System.out.println("Сумма расходов : " + out + " руб.");
-        System.out.println("Сумма доходов : " + in + " руб.");
+    }
+
+    private static void printSumOfIncome(List<Transaction> transactions) {
+        double sum = transactions.stream().mapToDouble(e -> e.getIncome()).sum();
+        System.out.println("Сумма доходов : " + sum + " руб.");
+    }
+
+    private static void printSumOfExpenses(List<Transaction> transactions) {
+        double sum = transactions.stream().mapToDouble(e -> e.getExpense()).sum();
+        System.out.println("Сумма расходов : " + sum + " руб.");
+    }
+
+    private static void printSummaryByGroups(List<Transaction> transactions) {
+        String shop;
+        double expense;
+        for (Transaction transaction : transactions) {
+            shop = transaction.getDescription();
+            expense = transaction.getExpense();
+            if (expense > 0) {
+                if (shops.containsKey(shop)) {
+                    shops.put(shop, shops.get(shop) + expense);
+                } else {
+                    shops.put(shop, expense);
+                }
+            }
+        }
         System.out.println("===========================================================");
         System.out.println("Суммы расходов по организациям :");
-        for (String str : shops.keySet()) {
-            System.out.println(str + "  = " + shops.get(str) + " руб.");
+        for (String shOP : shops.keySet()) {
+            System.out.println(shOP + "  = " + shops.get(shOP) + " руб.");
+        }
+    }
+
+    private static List<Transaction> parseCSV(Path csvFile) throws IOException {
+        final int COLUMNS_COUNT = 8;
+        List<Transaction> transactions = new ArrayList<>();
+        StringBuilder builder;
+        try (Scanner scanner = new Scanner(csvFile)) {
+            while (scanner.hasNext()) {
+                String line = scanner.nextLine();
+                if (line.replaceAll("\\D", "").equals("")) {
+                    continue; // пропускаем шапку таблицы и строки без данных
+                }
+                line = line.replaceAll("\"(\\d+),(\\d+)\"", "$1.$2");
+                double in, out;
+                String[] fragments = line.split(",");
+                if (fragments.length != COLUMNS_COUNT) {
+                    System.out.println("Wrong line: " + line);
+                    continue;
+                }
+                out = Double.parseDouble(fragments[COLUMNS_COUNT - 1]);
+                in = Double.parseDouble(fragments[COLUMNS_COUNT - 2]);
+                String[] segments = fragments[COLUMNS_COUNT - 3].split("    ");
+                String[] words = segments[1].split("[\\/\\\\]");
+                builder = new StringBuilder();
+                for (int i = 1; i < words.length; i++) {
+                    builder.append(words[i]);
+                    builder.append("\\");
+                }
+                transactions.add(new Transaction(builder.toString(), in, out));
+            }
+            return transactions;
+        }
+    }
+
+    private static List<Transaction> parseORG(Path orgFile) throws IOException {
+        final int COLUMNS_COUNT = 5;
+        List<Transaction> transactions = new ArrayList<>();
+        try (Scanner scanner = new Scanner(orgFile)) {
+            while (scanner.hasNext()) {
+                String line = scanner.nextLine();
+                if (line.replaceAll("\\D", "").equals("")) {
+                    continue; // пропускаем шапку таблицы и строки без данных
+                }
+                line = line.replaceAll("(\\d+),(\\d+)", "$1.$2");
+                double in, out;
+                String[] fragments = line.trim().split("\\s*\\|\\s*");
+                if (fragments.length != COLUMNS_COUNT + 1) {
+                    System.out.println("Wrong line: " + line);
+                    continue;
+                }
+                out = Double.parseDouble(fragments[COLUMNS_COUNT]);
+                in = Double.parseDouble(fragments[COLUMNS_COUNT - 1]);
+                transactions.add(new Transaction(fragments[COLUMNS_COUNT - 3], in, out));
+            }
+            return transactions;
         }
     }
 }
---
+++ 09_FilesAndNetwork/src/main/java/Transaction.java
@@ -0,0 +1,36 @@
+public class Transaction {
+
+    private String description;
+    private double income;
+    private double expense;
+
+    public Transaction(String description, double income, double expense) {
+        this.description = description;
+        this.income = income;
+        this.expense = expense;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getIncome() {
+        return income;
+    }
+
+    public void setIncome(double income) {
+        this.income = income;
+    }
+
+    public double getExpense() {
+        return expense;
+    }
+
+    public void setExpense(double expense) {
+        this.expense = expense;
+    }
+}
