b2413f53c9c517017a81194a441d04ebab0b71f4
Mammoth55 <slamam@mail.ru>
2020-10-28 22:26:20 +0600

    9.12 Домашняя работа 9.4 refactored v.2

--- 09_FilesAndNetwork/src/main/java/LentaParse.java
+++ 09_FilesAndNetwork/src/main/java/LentaParse.java
@@ -4,6 +4,10 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import java.io.*;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
 public class LentaParse {
 
@@ -11,14 +15,16 @@ public class LentaParse {
     private static final String FOLDER_PATH = "target/receivedImages/";
 
     public static void main(String[] args) {
-        String src;
         System.out.println("Download from  " + HTML_PATH);
+        Path path = Paths.get(FOLDER_PATH);
         try {
+            if (Files.notExists(path)) {
+                Files.createDirectory(path);
+            }
             Document doc  = Jsoup.connect(HTML_PATH).maxBodySize(0).get();
             Elements metaElements = doc.getElementsByTag("img");
             for (Element metaElement : metaElements) {
-                src = metaElement.absUrl("src");
-                getImage(src);
+                getImage(metaElement.absUrl("src"));
             }
             System.out.println("Done.");
         } catch (Exception ex) {
@@ -27,24 +33,14 @@ public class LentaParse {
     }
 
     private static void getImage(String src) throws IOException {
-        // Extract the name of the image from the src attribute
-        int indexName = src.lastIndexOf("/");
-        if (indexName == src.length()) {
-            src = src.substring(1, indexName);
-        }
-        String name = src.substring(indexName + 1, src.length());
+        String name = src.substring(src.lastIndexOf("/") + 1);
         if (name.endsWith(".jpg")) {
-            // Work with .jpg only !
-            URL url = new URL(src); // Open a URL Stream
-            InputStream in = url.openStream();
-            try (OutputStream out = new BufferedOutputStream(
-                    new FileOutputStream(FOLDER_PATH + name))) {
-                for (int b; (b = in.read()) != -1;) {
-                    out.write(b);
-                }
+            URL url = new URL(src);
+            try (InputStream in = url.openStream();
+                 OutputStream out = new BufferedOutputStream(
+                         new FileOutputStream(FOLDER_PATH + name))) {
+                in.transferTo(out);
                 System.out.println(name + "   Download success.");
-                out.close();
-                in.close();
             }
         }
     }
