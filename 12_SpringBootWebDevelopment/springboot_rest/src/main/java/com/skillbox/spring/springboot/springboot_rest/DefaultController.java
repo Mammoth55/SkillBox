812da7adfffcb8a66ad3d2b683c25fb3f96b8836
Mammoth55 <slamam@mail.ru>
2021-01-27 00:10:38 +0600

    12.4 Домашняя работа 12.1

---
+++ 12_SpringBootWebDevelopment/springboot_rest/src/main/java/com/skillbox/spring/springboot/springboot_rest/DefaultController.java
@@ -0,0 +1,14 @@
+package com.skillbox.spring.springboot.springboot_rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.Date;
+
+@RestController
+public class DefaultController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Current time : " + (new Date()).toString();
+    }
+}
