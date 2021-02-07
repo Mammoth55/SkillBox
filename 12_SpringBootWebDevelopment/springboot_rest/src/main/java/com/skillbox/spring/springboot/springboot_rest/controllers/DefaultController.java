ed87217f829eeacc2d7a4e5bbc50a7a84a9b41d5
Mammoth55 <slamam@mail.ru>
2021-02-08 02:28:35 +0600

    12.16 Домашняя работа 12.4

--- 12_SpringBootWebDevelopment/springboot_rest/src/main/java/com/skillbox/spring/springboot/springboot_rest/controllers/DefaultController.java
+++ 12_SpringBootWebDevelopment/springboot_rest/src/main/java/com/skillbox/spring/springboot/springboot_rest/controllers/DefaultController.java
@@ -1,14 +1,36 @@
 package com.skillbox.spring.springboot.springboot_rest.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import java.util.Date;
+import com.skillbox.spring.springboot.springboot_rest.model.Task;
+import com.skillbox.spring.springboot.springboot_rest.service.TaskService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import java.util.List;
 
-@RestController
+@Controller
 public class DefaultController {
 
-    @RequestMapping("/")
-    public String index() {
-        return "Current time : " + (new Date()).toString();
+    private final TaskService taskService;
+
+    public DefaultController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+    @GetMapping("/")
+    public String index(Model model) {
+        List<Task> allTasks = taskService.getAllTasks();
+        model.addAttribute("allTasks", allTasks);
+        model.addAttribute("tasksCount", allTasks.size());
+        return "index";
+    }
+
+    @GetMapping("/hello")
+    public String helloPage(Model model) {
+        return "hello";
+    }
+
+    @GetMapping("/goodbye")
+    public String goodbyePage(Model model) {
+        return "goodbye";
     }
 }
