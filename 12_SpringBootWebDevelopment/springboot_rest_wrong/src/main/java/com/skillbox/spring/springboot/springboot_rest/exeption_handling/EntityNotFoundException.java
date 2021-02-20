4fee0c46d3fc5e542a073f8e066ea43194c6f71b
Mammoth55 <slamam@mail.ru>
2021-02-20 19:29:57 +0600

    12.16 Домашняя работа 12.4 final

--- 12_SpringBootWebDevelopment/springboot_rest_wrong/src/main/java/com/skillbox/spring/springboot/springboot_rest/exeption_handling/EntityNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.skillbox.spring.springboot.springboot_rest.exeption_handling;
-
-public class EntityNotFoundException extends RuntimeException {
-
-    public EntityNotFoundException(String message) {
-        super(message);
-    }
-}
--- 12_SpringBootWebDevelopment/springboot_rest_wrong/src/main/java/com/skillbox/spring/springboot/springboot_rest/repository/TaskRepository.java
+++
@@ -1,8 +0,0 @@
-package com.skillbox.spring.springboot.springboot_rest.repository;
-
-import com.skillbox.spring.springboot.springboot_rest.model.Task;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TaskRepository extends JpaRepository<Task, Integer> {
-
-}
