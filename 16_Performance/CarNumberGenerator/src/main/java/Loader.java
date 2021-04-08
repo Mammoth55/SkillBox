cb39dac5744e375f925a1e5157b644f8b2dcecf5
slamam@mail.ru <3141592653589+Pi>
2021-04-08 21:00:23 +0600

    16.7 Домашняя работа 16.1

---
+++ 16_Performance/CarNumberGenerator/src/main/java/Loader.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+import java.util.List;
+
+// первичный замер 56 сек/регион (среднее из трех замеров)
+// после рефакторинга padNumber (String => StringBuilder) = 56 сек/регион, т.к. выигрыш без конкатенаций тут несущественный
+// после рефакторинга main (String => StringBuilder) = 56 сек/регион, т.к. выигрыш без конкатенаций тут несущественный
+// после рефакторинга main (FileOutputStream => PrintWriter) = 1.9 сек/регион,
+//      т.к. вывод буферизирован и выгрузка буфера оптимизирована
+// перекалибровка на 32 региона = 41 сек
+// после рефакторинга main (Multithreading => 4 потоков + 4 файлов) = 20 сек,
+//      т.к. 4 ядра CPU и распределение вычислений между ними
+// после рефакторинга main (Multithreading => 8 потоков + 8 файлов) = 19 сек,
+//      т.к. 4 ядра CPU и распределение вычислений между ними
+// после рефакторинга main (Multithreading => 16 потоков + 16 файлов) = 22 сек,
+//      т.к. 4 ядра CPU и распределение вычислений между ними
+// Т.о. по итогу вероятно оптимальное кол-во потоков это 2 для каждого физического ядра CPU
+//      при распределении работы для достижения макс.эффективности
+
+public class Loader {
+
+    static final int NUMBER_CAPACITY = 1000;
+    static final int REGION_CAPACITY = 192;
+    static final char[] LETTERS = {'У', 'К', 'Е', 'Н', 'Х', 'В', 'А', 'Р', 'О', 'С', 'М', 'Т'};
+    static final String FILE_NAME = "src/main/resources/numbers";
+    static final String FILE_EXT = ".txt";
+
+    public static void main(String[] args) throws Exception {
+        List<Thread> threads = new ArrayList<>();
+        System.out.println("Program started...");
+        int maxThread = Math.min(REGION_CAPACITY, 2 * Runtime.getRuntime().availableProcessors());
+        long start = System.currentTimeMillis();
+        for (int count = 0; count < maxThread; count++) {
+            Thread thread = new Thread(new MyThread(count * REGION_CAPACITY / maxThread,
+                    REGION_CAPACITY / maxThread,
+                    FILE_NAME + Integer.toString(count) + FILE_EXT));
+            threads.add(thread);
+            thread.start();
+        }
+        for (Thread thread : threads) {
+            thread.join();
+        }
+        System.out.println("Program ended in " + (System.currentTimeMillis() - start) + " ms");
+    }
+}
---
+++ 16_Performance/CarNumberGenerator/src/main/java/MyThread.java
@@ -0,0 +1,60 @@
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
+public class MyThread implements Runnable {
+
+    private final int regionStart;
+    private final int regionCount;
+    private final String fileName;
+
+    public MyThread(int regionStart, int regionCount, String fileName) {
+        this.regionStart = regionStart;
+        this.regionCount = regionCount;
+        this.fileName = fileName;
+    }
+
+    @Override
+    public void run() {
+        PrintWriter writer = null;
+        System.out.println(Thread.currentThread().getName() + " in progress...");
+        try {
+            writer = new PrintWriter(fileName);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        for (int regionCode = regionStart; regionCode < regionStart + regionCount; regionCode++) {
+            if (regionCode == 0) {
+                continue;
+            }
+            StringBuilder builder = new StringBuilder();
+            for (int number = 1; number < Loader.NUMBER_CAPACITY; number++) {
+                for (char firstLetter : Loader.LETTERS) {
+                    for (char secondLetter : Loader.LETTERS) {
+                        for (char thirdLetter : Loader.LETTERS) {
+                            builder.append(firstLetter);
+                            builder.append(padNumber(number, 3));
+                            builder.append(secondLetter);
+                            builder.append(thirdLetter);
+                            builder.append(padNumber(regionCode, 2));
+                            builder.append('\n');
+                        }
+                    }
+                }
+            }
+            writer.write(builder.toString());
+        }
+        writer.flush();
+        writer.close();
+    }
+
+    private static StringBuilder padNumber(int number, int numberLength) {
+        StringBuilder builder = new StringBuilder(Integer.toString(number));
+        int padSize = numberLength - builder.length();
+        StringBuilder zeros = new StringBuilder(3);
+        if (padSize > 0) {
+            zeros.append("0".repeat(padSize));
+        }
+        zeros.append(builder);
+        return zeros;
+    }
+}
